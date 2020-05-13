c547574f0bfa8eb88ef87fc9023e84895d15b1db
Ted Zyzsdy <zyzsdy@gmail.com>
2020-05-13 17:12:51 +0800

    完成框架

---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="topnavbar">
+    <el-menu mode="horizontal" :default-active="activeIndex" background-color="#555555" text-color="#FFFFFF" active-text-color="#F789D4">
+      <el-menu-item index="home">主页</el-menu-item>
+      <el-menu-item>登录</el-menu-item>
+      <el-menu-item>注册</el-menu-item>
+    </el-menu>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TopNavbar extends Vue {
+    @Prop() private activeIndex!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.topnavbar{
+  height: 60px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  background-color: #555555;
+}
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import '../theme/ccxc-dark.scss'
+
+Vue.use(Element)
